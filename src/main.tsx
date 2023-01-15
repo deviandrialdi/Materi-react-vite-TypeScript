acd47d7bc58ec65d162d42b54cd301eb92e7c939
Devi Andri Aldi <deviandrialdi@gmail.com>
2023-01-15 09:49:17 +0700

    Day 1 edit button, nambah class dan function

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Function from "./pages/Function";
+import Class from "./pages/Class";
+import "./styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Class />
+  </React.StrictMode>
+);
---
+++ src/pages/Function.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import Button from '../components/Button';
+import '../styles/App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  function clickHandler () {
+    setCount((count) => count + 1);
+  }
+
+  return (
+    <div className="App">
+      <div className="flex justify-center">
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 style={{color: "yellow"}}>Vite + React</h1>
+      <div className="card">
+        <Button 
+        label={`count is ${count}`}
+        onClick= {() => clickHandler }/>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
